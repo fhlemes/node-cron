bc3e7c63e83dad9d37c56a5b9d8182e97d105f74
fhlemes <fhlemes@gmail.com>
2019-01-31 00:29:23 -0200

    ajustado a função

--- index.js
+++ index.js
@@ -3,26 +3,16 @@ const cron = require("node-cron");
 
 app = express();
 
-// cada um minuto e 30 segundos executa
-/*
- * Uma vez a cada 5 minutos = (* /5 * * * *)
- * Duas vezes por hora = (* 0,30 * * * *)
- * Uma vez por hora = (* 0 * * * *)
- * Duas vezes por dia = (* 0 0,12 * *)
- * Uma vez por dia = (* 0 0 * * *)
- * Uma vez por semana = (* 0 0 * * 0)
- * Uma vez por mês = (* 0 0 1 * *)
- */
-
 verifyNumber = () => {
   let n = 1;
   if (2 === n) {
-    console.log("O numero está certinho");
+    console.log("O numero é igual a 2");
   } else {
-    console.log("Numero não é igual");
+    console.log("O numero passado não é igual a 2");
   }
 };
 
+// executa uma verificação a cada 5 segundos
 cron.schedule("*/5 * * * * *", () => {
   verifyNumber();
 });
